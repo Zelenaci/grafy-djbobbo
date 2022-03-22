229c634a0519903a72e86dde0adbb4400bf05467
djbobbo <bohous.steier@seznam.cz>
2022-03-22 01:17:14 +0100

    Jeste to neni

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python3
 
 
-with open("soubor.txt", "r") as f:
-    pass
+#with open("soubor.txt", "r") as f:
+    #pass
+
+import numpy as np
+import matplotlib.pyplot as plt
+from math import pi
+
+jmeno = str(input("zadej název grafu: "))
+frekvence = int(input("zadej frekvenci: "))
+amplituda = float(input("zadej amplitudu: "))
+doba = int(input("pocet period: "))
+offsety = float(input("posun osa y: "))
+posun = float(input("fázový posun: "))
+
+t = np.linspace(0, doba*1/frekvence, frekvence*10000)
+u = amplituda * (np.sin(2*pi*frekvence*t + np.deg2rad(posun))) + offsety
+
+
+plt.plot(t, u, label = jmeno)
+plt.xlabel
+plt.grid()
+plt.legend()
+
+
+plt.show()
